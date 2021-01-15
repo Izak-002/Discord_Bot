9c8565330e57c5dd4c71abc5a3f3395286e83824
Izak Le Roux <izaklerouxx002@gmail.com>
2021-01-15 23:40:05 +0200

    shows random gifs for selected keywords

--- Bot.py
+++ Bot.py
@@ -31,6 +31,17 @@ def search_gifs(query):
         return "Exception when calling DefaultApi->gifs_search_get: %s\n" % e
 
 
+# this is the response with gif function
+def gif_response(input):
+
+    gifs = search_gifs(input)
+    lst = list(gifs.data)
+    gif = random.choice(lst)
+
+    # gif = gifs.data[5]
+    return gif.url
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {}'.format(client.user))
@@ -46,6 +57,10 @@ async def on_message(message):
 
     if message.content.startswith('uwu'):
         await message.channel.send('ara ara onii-chan')
+        await message.channel.send(gif_response('anime'))
+
+    if message.content.startswith('$rikka'):
+        await message.channel.send(gif_response('rikka'))
 
 
 @client.event
@@ -58,6 +73,10 @@ async def on_member_join(member):
 
     role = discord.utils.get(member.guild.roles, name="Nani?")
     await member.add_roles(role, reason=None, atomic=True)
+
+    gifs = api_instance.gifs_search_get(giphy_api_key, 'loli', rating='g')
+    gif = gifs.data[4]
+    await member.dm_channel.send(gif.url)
 
 
 client.run(TOKEN)
