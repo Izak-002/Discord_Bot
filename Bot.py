28a1e82e07e91065efd2b7a13a8b2df30bdbfea1
Izak Le Roux <izaklerouxx002@gmail.com>
2021-01-19 20:27:29 +0200

    Added random quote response to bot

--- Bot.py
+++ Bot.py
@@ -4,6 +4,8 @@ import random
 import giphy_client
 from giphy_client.rest import ApiException
 from dotenv import load_dotenv
+import requests
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,7 +24,7 @@ api_instance = giphy_client.DefaultApi()
 
 # make bot prefix-------------------------------------
 from discord.ext import commands
-help_category = commands.DefaultHelpCommand(no_category='Commands of server')
+help_category = commands.DefaultHelpCommand(no_category='Commands of Bot')
 bot = commands.Bot(command_prefix='$',
                    help_command=help_category,
                    intents=intents)
@@ -52,6 +54,14 @@ def gif_response(input):
 
     # gif = gifs.data[5]
     return gif.url
+
+
+# this is to get the quote
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + ' - said by ' + json_data[0]['a']
+    return quote
 
 
 @bot.event
@@ -86,16 +96,15 @@ async def on_message(message):
 
 # bot commands---------------------------------------
 
-
-@bot.command(
-    # ADDS THIS VALUE TO THE $HELP PING MESSAGE.
-    help=
-    "Uses come crazy logic to determine if pong is actually the correct value or not.",
-    # ADDS THIS VALUE TO THE $HELP MESSAGE.
-    brief="Prints pong back to the channel.")
-async def ping(ctx):
-    # SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
-    await ctx.channel.send("pong")
+# @bot.command(
+# ADDS THIS VALUE TO THE $HELP PING MESSAGE.
+# help=
+# "Uses come crazy logic to determine if pong is actually the correct value or not.",
+# ADDS THIS VALUE TO THE $HELP MESSAGE.
+# brief="Prints pong back to the channel.")
+# async def ping(ctx):
+# SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
+# await ctx.channel.send("pong")
 
 
 @bot.command(help='Just use $rikka to activate command',
@@ -105,8 +114,9 @@ async def rikka(ctx):
     await ctx.channel.send(gif_response('rikka'))
 
 
-@bot.command(help='use $pat and someones username to actiavte command',
-             brief='when used it wil send a headpat gif to someone ^-^')
+@bot.command(
+    help='use $pat and someones username to actiavte command e.g. [$pat @jack]',
+    brief='when used it wil send a headpat gif to someone ^-^')
 async def pat(ctx, user: discord.Member):
 
     gifs = api_instance.gifs_search_get(giphy_api_key,
@@ -116,6 +126,28 @@ async def pat(ctx, user: discord.Member):
     gif = gifs.data[1]
     await ctx.channel.send(f'{ctx.author.name} pats {user.mention}\n')
     await ctx.channel.send(gif.url)
+
+
+@bot.command(
+    help='use $hug and someones username to activate command e.g. [$hug @jack]',
+    brief='When used it wil send a hug gif to someone :)')
+async def hug(ctx, user: discord.Member):
+
+    gifs = api_instance.gifs_search_get(giphy_api_key,
+                                        'Anime hug',
+                                        limit=50,
+                                        rating='g')
+    gif = gifs.data[16]
+    await ctx.channel.send(f'{ctx.author.name} hugs {user.mention}\n')
+    await ctx.channel.send(gif.url)
+
+
+@bot.command(help='use $wise to get random quote',
+             brief='When used it will show a random quote')
+async def wise(ctx):
+
+    quote = get_quote()
+    await ctx.channel.send(quote)
 
 
 # -----------------------------------------------------
